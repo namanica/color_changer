794e769502dafadbee3896089fafbdc0564443e7
namanica <veronichkapilipna@gmail.com>
2023-12-30 20:01:07 +0200

    added locking and copying functions

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const columns = document.querySelectorAll('.column');
 
 document.addEventListener('keydown', event => {
+  event.preventDefault();
   if (event.code.toLowerCase() === 'space') {
     SetRandomColors();
   }
 })
 
 document.addEventListener('click', event => {
-  
+  const type = event.target.dataset.type;
+
+  if (type === 'lock') {
+    const node = event.target.tagName.toLowerCase() === 'i' ? event.target : event.target.children[0];
+
+    node.classList.toggle('fa-lock-open');
+    node.classList.toggle('fa-lock');
+  }
+  else if (type === 'copy') {
+    CopyByClick(event.target.textContent);
+  }
 })
 
 function GenerateRandomColor() {
@@ -21,21 +32,28 @@ function GenerateRandomColor() {
 
 function SetRandomColors() {
   columns.forEach((column) => {
+    const isLocked = column.querySelector('i').classList.contains('fa-lock');
     const colorName = column.querySelector('h2');
     const button = column.querySelector('button');
     const generatedColor = GenerateRandomColor();
 
+    if (isLocked) return;
+
     colorName.textContent = generatedColor;
     column.style.background = generatedColor;
 
-    setTextColor(colorName, generatedColor);
-    setTextColor(button, generatedColor);
+    SetTextColor(colorName, generatedColor);
+    SetTextColor(button, generatedColor);
   })
 }
 
 SetRandomColors();
 
-function setTextColor(text, color) {
+function SetTextColor(text, color) {
   const luminance = chroma(color).luminance();
   text.style.color = luminance > 0.5 ? 'black' : 'white';
+}
+
+function CopyByClick(text) {
+  return navigator.clipboard.writeText(text);
 }
